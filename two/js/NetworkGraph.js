5a18b09ac9556951b65d28372b03716c68abf1a7
Layla R. Lajami <Layla.R.Lajami@hotmail.com>
2014-04-11 07:55:51 -0400

    - removed the date slider - used a fixed circular layout - added node attributes

--- two/js/NetworkGraph.js
+++ two/js/NetworkGraph.js
@@ -11,8 +11,7 @@ function update(date, hour_minute_0, hour_minute_1, tooltip)
             height = 500,
             color = d3.scale.category20c();
 
-        var nodes = {};
-        
+        var nodes = []; 
         var filterd_links = links.filter(bound_data);
         var length = filterd_links.length;
 
@@ -49,22 +48,71 @@ function update(date, hour_minute_0, hour_minute_1, tooltip)
             } 
         }
 
-        var force = d3.layout.force()
-            .nodes(d3.values(nodes))
-            .links(filterd_links)
-            .size([width, height])
-            .linkDistance(90)
-           // .linkDistance(function(d){return d.value;})
-            .gravity(0.06)
-            .charge(-300)
-            .friction(.5)
-            .size([width/2 , height ])
-            .on("tick", tick)
-            .start();
+        for(var i = 1; i < 26; i++)
+        {
+            if(typeof nodes[i] == 'undefined')
+            {   
+                var node = {name : ""+i};
+                nodes[i] = node;
+            }
+            nodes[i].out_degree = 0;
+            nodes[i].in_degree = 0;
+            nodes[i].outgoing_neighbors = [];
+            nodes[i].incoming_neighbors = [];
+            nodes[i].sum_duration = 0;
+            nodes[i].sum_avg_duration = 0;
+        }
+        
+        for(var j = 0; j < filterd_links.length; j++)
+        {
+            nodes[filterd_links[j].source.name].out_degree++;
+            nodes[filterd_links[j].target.name].in_degree++;
+            nodes[filterd_links[j].source.name].sum_duration += filterd_links[j].total_duration;
+            nodes[filterd_links[j].source.name].sum_avg_duration += filterd_links[j].value;
+            nodes[filterd_links[j].source.name].outgoing_neighbors.push(filterd_links[j].target.name);
+            nodes[filterd_links[j].target.name].incoming_neighbors.push(filterd_links[j].source.name);
+        }
+
+        // setting the locations of the nodes on the circumference of a circle of radius = 200
+        var radius = 200,
+            theta = 0,
+            new_theta = 0,
+            d_theta = 360.0/25.0;
+
+        for(var i = 1; i < nodes.length; i++)
+        {
+            new_theta = toRadians(theta);
+            theta += d_theta;
+            nodes[i].x  = radius * ( Math.cos(new_theta) + 1) + 50;
+            nodes[i].px = radius * ( Math.cos(new_theta) + 1) + 50;
+            nodes[i].y  = radius * (-Math.sin(new_theta) + 1) + 50;
+            nodes[i].py = radius * (-Math.sin(new_theta) + 1) + 50;
+            nodes[i].fixed = true;
+        }
+
+        var scale = d3.scale.linear()
+                      .domain(scaleBounds(filterd_links))
+                      .range([50,100]);
+
+        var nodeScale = d3.scale.linear()
+                      .domain(scaleBounds2(nodes))
+                      .range([4,9]);
+
          
         var svg = d3.select("#vis").append("svg")
             .attr("width", width)
             .attr("height", height);
+
+        var force = d3.layout.force()
+            .nodes(d3.values(nodes))
+            .links(filterd_links)
+            .size([width, height])
+            //.linkDistance(function(d){return scale(d.value);})
+            .gravity(0)
+            .charge(-60)
+            .friction(.5)
+            .on("tick", tick)
+            .start();
          
         // build the arrow.
         svg.append("svg:defs").selectAll("marker")
@@ -100,7 +148,7 @@ function update(date, hour_minute_0, hour_minute_1, tooltip)
          
         // add the nodes
         node.append("circle")
-            .attr("r", 5)
+            .attr("r", function(d){return nodeScale(d.out_degree);})
             .style("fill", function(d) { return color(d.name); });
 
          
@@ -132,7 +180,7 @@ function update(date, hour_minute_0, hour_minute_1, tooltip)
         }
          
         // action to take on mouse click
-        function click() 
+        function click(d,i) 
         {
             d3.select(this).select("text").transition()
                 .duration(750)
@@ -143,20 +191,22 @@ function update(date, hour_minute_0, hour_minute_1, tooltip)
             d3.select(this).select("circle").transition()
                 .duration(750)
                 .attr("r", 16);
+            show_details2(d, i, d3.select(this));
         }
          
         // action to take on mouse double click
-        function dblclick() 
+        function dblclick(d,i) 
         {
             d3.select(this).select("circle").transition()
                 .duration(750)
-                .attr("r", 6);
+                .attr("r", function(d){return nodeScale(d.out_degree);})
             d3.select(this).select("text").transition()
                 .duration(750)
                 .attr("x", 12)
                 .style("stroke", "none")
                 .style("fill", "black")
                 .style("font", "10px sans-serif");
+            hide_details2(d, i, d3.select(this));
         }
 
         function bound_data(d)
@@ -176,8 +226,28 @@ function update(date, hour_minute_0, hour_minute_1, tooltip)
             content +="<span class=\"type\"><b>Average Duration:</b></span><span class=\"value\"> " + addCommas(d.value.toFixed(2)) + " seconds</span><br/>";
             content +="<span class=\"type\"><b>#Connections:</b></span><span class=\"value\"> " + d.num_connections + "</span><br/>";
             content +="<span class=\"type\"><b>Date Start:</b></span><span class=\"value\"> " + d.date_start + "</span><br/>";
+            content +="<span class=\"type\"><b>Time Start:</b></span>";
             content += stringifyArray(d.starting_time);
             tooltip.showTooltip(content, d3.event);
+        }
+
+        function show_details2(d, i, element) 
+        {           
+            var content = "<span class=\"type\"><b>Name:</b></span><span class=\"value\"> " + d.name + "</span><br/>";
+            content +="<span class=\"type\"><b>Out Degree:</b></span><span class=\"value\"> " + d.out_degree + "</span><br/>";
+            content +="<span class=\"type\"><b>In Degree:</b></span><span class=\"value\"> " + d.in_degree + "</span><br/>";
+            content +="<span class=\"type\"><b>Sum Duration:</b></span><span class=\"value\"> " + addCommas(d.sum_duration) + " seconds</span><br/>";
+            content +="<span class=\"type\"><b>Sum Avg Duration:</b></span><span class=\"value\"> " + addCommas(d.sum_avg_duration.toFixed(2)) + " seconds</span><br/>";
+            content +="<span class=\"type\"><b>Outgoing Neighbors:</b></span>";
+            content += stringifyArray(d.outgoing_neighbors);
+            content +="<br/><span class=\"type\"><b>Incoming Neighbors:</b></span>";
+            content += stringifyArray(d.incoming_neighbors);
+            tooltip.showTooltip(content, d3.event);
+        }
+
+        function hide_details2(d, i, element) 
+        {
+            tooltip.hideTooltip();
         }
 
         function hide_details(d, i, element) 
@@ -189,12 +259,59 @@ function update(date, hour_minute_0, hour_minute_1, tooltip)
 
         function stringifyArray(array)
         {
-            var content ="<span class=\"type\"><b>Time Start:</b></span><span class=\"value\"> " + array[0] + "</span>";
-            for(var i =1; i < array.length; i++)
+            if(array.length > 0)
             {
-                content += "<span class=\"type\">, </span><span class=\"value\"> " + array[i] + "</span>";
+                var content = "<span class=\"type\"></span><span class=\"value\"> " + array[0] + "</span>";
+                for(var i =1; i < array.length; i++)
+                {
+                    content += "<span class=\"type\">, </span><span class=\"value\"> " + array[i] + "</span>";
+                }
+                return content;
             }
-            return content;
+            else
+                return "";
+        }
+
+        function scaleBounds(array)
+        {
+            if(array.length > 0)
+            {
+                var max = array[0].value,
+                    min = array[0].value;
+                for(var i = 1; i < array.length; i++)
+                {
+                    if(array[i].value > max)
+                        max = array[i].value;
+                    if(array[i].value < min)
+                        min = array[i].value;
+                }
+                return [min,max]; 
+            }
+            else return [0,0];
+        }
+
+        function scaleBounds2(array)
+        {
+            if(array.length > 0)
+            {
+                var max = array[1].out_degree,
+                    min = array[1].out_degree;
+                for(var i = 2; i < array.length; i++)
+                {
+                 if(array[i].out_degree > max)
+                     max = array[i].out_degree;
+                 if(array[i].out_degree < min)
+                     min = array[i].out_degree;
+                }
+                return [min,max]; 
+            }
+            else
+                return [0,0]; 
+        }
+
+        function toRadians (angle) 
+        {
+            return angle * (Math.PI / 180.0);
         }
     });  
 }
